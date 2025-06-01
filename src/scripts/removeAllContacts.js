ab931e2b553bb6c7729bf74890bf5321e1d060c0
Angelina <kolisnuk.a12@gmail.com>
2025-06-01 18:36:02 +0300

    add hw

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,15 @@
-export const removeAllContacts = async () => {};
+import fs from 'fs/promises';
+import path from 'path';
+
+const dbPath = path.resolve('src/db/db.json');
+
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(dbPath, JSON.stringify([]));
+    console.log('Усі контакти видалено.');
+  } catch (error) {
+    console.error('Помилка при видаленні контактів:', error);
+  }
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,23 @@
-export const removeLastContact = async () => {};
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts.length === 0) {
+      console.log('Контактів немає для видалення.');
+      return;
+    }
+
+    contacts.pop();
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log('Останній контакт видалено.');
+  } catch (error) {
+    console.error('Помилка при видаленні останнього контакту:', error);
+  }
+};
 
 removeLastContact();
